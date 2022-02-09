57580432b92edc4383e63157a4394cb22cc526ec
unknown <mathias.greenhalgh@gmail.com>
2022-02-09 11:59:35 +0100

    Working on the final details

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,20 +1,20 @@
 
 let link = document.querySelector(".elBurgerito")
 let contact = document.querySelector(".burger-popup")
-let overlay = document.querySelector(".overlay")
+let overlay = document.querySelector(".overloff")
 
 
 link.addEventListener('click', function() {
     contact.classList.toggle("active");
+    link.classList.toggle("active");
     overlay.classList.toggle("overlon");
     
 });
 
 overlay.addEventListener('click', function() {
     contact.classList.toggle("active");
+    link.classList.toggle("active");
     overlay.classList.toggle("overlon");
 });
 
-
-// smooth scroll
 
